78c69d02db39b94a3ae1c458941e1621e63329c1
Arlene Andrews <kara_arlene@yahoo.com>
2021-02-23 18:26:21 -0700

    Test to see if the dict is empty

--- whereisit.py
+++ whereisit.py
@@ -1,6 +1,13 @@
 # List item -input new ones?
+
+# test - list the items in the dictionary
+itemFind = {}
+if len(itemFind)< 1:
+  print("You have no items listed! Ready to add them.")
 # Get item to find
 # Return location of item
 # Another item?
 # Update item location?
 # Quit
+
+itemFind = {}
