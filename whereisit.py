c27a7e97e312ff1005b23f1628e969bcb8e36148
Arlene Andrews <kara_arlene@yahoo.com>
2021-02-23 19:15:24 -0700

    Error - not sure why - research time!

--- whereisit.py
+++ whereisit.py
@@ -1,16 +1,14 @@
-# List item -input new ones?
-
-# test - list the items in the dictionary
-itemFind = {"item":"shoes", "find":"floor"}
+itemFind = {}
 if len(itemFind)> 1:
   print("Items already listed:")
-  print(itemFind["item"])
+  for x in itemFind.keys():
+    print(x)
 else:
   print("You have no items listed! Ready to add them.")
+# List item -input new ones?
+# test - list the items in the dictionary
 # Get item to find
 # Return location of item
 # Another item?
 # Update item location?
 # Quit
-
-itemFind = {}
