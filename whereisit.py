adf1b812ba9e179b335e41a91adf556bd722039f
Arlene Andrews <kara_arlene@yahoo.com>
2021-02-23 20:56:37 -0700

    Ready to test inputs!

--- whereisit.py
+++ whereisit.py
@@ -1,3 +1,5 @@
+import pyinputplus as pyip
+
 itemFind = {}
 if len(itemFind)>= 1:
   print("Items already listed:")
@@ -5,6 +7,18 @@ if len(itemFind)>= 1:
     print(x)
 else:
   print("You have no items listed! Ready to add them.")
+  while True:
+    print("Enter the item's name - it must be unique")
+    item = input()
+    try
+      if len(item) < 1 or len(item) > 50:
+        print('This item must have a length between 1 and 50 characters.')
+      else
+      print('Please list where the item can be found. This alos must be unique.')
+      find = input()
+      try
+      if len(find) < 1 or len(find) > 50:
+        print('The location must have a length between 1 and 50 characters.')
 # List item -input new ones?
 # test - list the items in the dictionary
 # Get item to find
