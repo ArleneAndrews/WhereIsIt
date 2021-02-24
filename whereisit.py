1c45411844be379bb96f33dd2c86e8627f76e964
Arlene Andrews <kara_arlene@yahoo.com>
2021-02-23 18:39:00 -0700

    Test #2 with sample data

--- whereisit.py
+++ whereisit.py
@@ -1,8 +1,11 @@
 # List item -input new ones?
 
 # test - list the items in the dictionary
-itemFind = {}
-if len(itemFind)< 1:
+itemFind = {"item":"shoes", "find":"floor"}
+if len(itemFind)> 1:
+  print("Items already listed:")
+  print(itemFind["item"])
+else:
   print("You have no items listed! Ready to add them.")
 # Get item to find
 # Return location of item
