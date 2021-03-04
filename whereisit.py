12527dc6206d67443138302fa16333e6bbb202e3
Arlene Andrews <kara_arlene@yahoo.com>
2021-03-04 13:15:58 -0700

    Items and locations added to dictionary

--- whereisit.py
+++ whereisit.py
@@ -1,26 +1,39 @@
 import pyinputplus as pyip
+quitStr = 'None'
 
 itemFind = {}
-if len(itemFind)>= 1:
+def itemList():
   print("Items already listed:")
   for x in itemFind.keys():
     print(x)
+# test - list the items in the dictionary
+if len(itemFind)>= 1:
+  itemList()
 else:
+# List item -input
+
   print("You have no items listed! Ready to add them.")
   while True:
-    print("Enter the item's name - it must be unique")
+    print("Enter the item's name - or type NONE to quit.")
     item = input()
-    try
-      if len(item) < 1 or len(item) > 50:
-        print('This item must have a length between 1 and 50 characters.')
-      else
-      print('Please list where the item can be found. This alos must be unique.')
-      find = input()
-      try
-      if len(find) < 1 or len(find) > 50:
-        print('The location must have a length between 1 and 50 characters.')
-# List item -input new ones?
-# test - list the items in the dictionary
+    if len(item) < 1 or len(item) > 50:
+      print('This item must have a length between 1 and 50 characters.')
+    elif item.casefold() == quitStr.casefold():
+      itemList()    
+      break  
+    else:
+      print('Please list where the item can be found.')
+    find = input()
+    
+    if len(find) < 1 or len(find) > 50:
+      print('The location must have a length between 1 and 50 characters.')
+    else:
+      newItem = {item:find}
+      print(newItem)
+      itemFind.update({item:find})
+      itemList()
+      
+
 # Get item to find
 # Return location of item
 # Another item?
