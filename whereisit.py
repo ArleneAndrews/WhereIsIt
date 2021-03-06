4fdcf3c0eec8f30c10728f3e1068fd8375380263
Arlene Andrews <kara_arlene@yahoo.com>
2021-03-06 11:19:36 -0700

    Starting find function, not correct

--- whereisit.py
+++ whereisit.py
@@ -1,40 +1,51 @@
 import pyinputplus as pyip
-quitStr = 'None'
 
 itemFind = {}
+
+def yesNo():
+  answer = input('Any more to list? Yes or No.  ').upper()
+  if answer == 'NO' or answer =='N':
+    itemList()
+      else:
+    addStuff()
+
+def addStuff():
+  item = input("Enter the item's name. ")
+  if len(item) < 1 or len(item) > 50:
+    print('This item must have a length between 1 and 50 characters.')
+  else:
+    #Get input of where the item is found
+    find = input('Please list where the item can be found. ')
+  
+  if len(find) < 1 or len(find) > 50:
+    print('The location must have a length between 1 and 50 characters.')
+  else:
+    itemFind.update({item:find})
+    yesNo()
+    
+
 def itemList():
   print("Items already listed:")
   for x in itemFind.keys():
     print(x)
-# test - list the items in the dictionary
+
+def itemLost(lostItem):
+  for key, value in itemFind.keys():
+    if lostItem == key:
+        return value
+    return "This item hasn't been entered."
+
 if len(itemFind)>= 1:
   itemList()
 else:
 # List item -input
-
   print("You have no items listed! Ready to add them.")
-  while True:
-    print("Enter the item's name - or type NONE to quit.")
-    item = input()
-    if len(item) < 1 or len(item) > 50:
-      print('This item must have a length between 1 and 50 characters.')
-    elif item.casefold() == quitStr.casefold():
-      itemList()    
-      break  
-    else:
-      print('Please list where the item can be found.')
-    find = input()
-    
-    if len(find) < 1 or len(find) > 50:
-      print('The location must have a length between 1 and 50 characters.')
-    else:
-      newItem = {item:find}
-      print(newItem)
-      itemFind.update({item:find})
-      itemList()
-      
-
+  addStuff()
+  yesNo()
 # Get item to find
+lostItem = ('What are you looking for?  ')
+hereItIs = itemLost(lostItem)
+print(hereItIs)
 # Return location of item
 # Another item?
 # Update item location?
